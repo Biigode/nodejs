80b71206f1f5696867b8af6c3589b7818ac8ecfd
victor almeida de freitas <victor.freitas08@hotmail.com>
2023-03-16 21:59:13 -0300

    disable users route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://mongodb:27017/nest'),
-    UsersModule,
-  ],
+  // imports: [
+  //   MongooseModule.forRoot('mongodb://mongodb:27017/nest'),
+  //   UsersModule,
+  // ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
